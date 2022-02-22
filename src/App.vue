fd1f750b09d151b0acb1f08e43c97917d1f53e2a
JIAHANG <1173574513@qq.com>
2022-02-22 16:55:13 +0800

    完成用户列表功能开发

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,7 @@
 
 <script>
 
-export default {
+export default { 
   name: 'app',
   components: {
     
--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from '../components/Login.vue'
 import Home from '../components/Home.vue'
+import Welcome from '../components/Welcome.vue'
+import Users from '../components/users/Users.vue'
 
 Vue.use(VueRouter)
 
@@ -21,7 +23,21 @@ const router = new VueRouter({
     {
       path: '/home',
       name: 'Home',
-      component: Home
+      component: Home,
+      redirect: '/welcome',
+      children: [
+        {
+          path: '/welcome',
+          name: 'Welcome',
+          component: Welcome,
+        },
+        {
+          path: '/users',
+          name: 'Users',
+          component: Users,
+        }
+
+      ]
     }
   ]
 })
