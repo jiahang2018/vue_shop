458c2c5751582639932a9376c7a4fdad11b68f82
JIAHANG <1173574513@qq.com>
2022-03-01 09:26:09 +0800

    完成图表功能开发

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ import Params from '../components/goods/Params.vue'
 import List from '../components/goods/List.vue'
 import Add from '../components/goods/Add.vue'
 import Orders from '../components/order/Order.vue'
+import Report from '../components/report/Report.vue'
 
 Vue.use(VueRouter)
 Vue.use(Rights)
@@ -78,6 +79,11 @@ const router = new VueRouter({
           path: '/orders',
           name: 'Orders',
           component: Orders,
+        },
+        {
+          path: '/reports',
+          name: 'Report',
+          component: Report,
         }
 
       ]
